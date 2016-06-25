3d6dab00dcf98597daaa8dfbfd26017a0f0ce609
Koji23 <jordanchong23@gmail.com>
2016-06-24 20:05:20 -0700

    Begin changeVideo method on App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,43 @@
-var App = (props) => (
-  <div>
-    <h1>testing</h1>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={props.videos[0]}/>
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={props.videos}/>
-    </div>
-  </div>
-);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      video: this.props.videos[0],
+      videos: this.props.videos
+    };
+  }
+
+  //initilization fn setState
+  //event handler for changing state
+
+  _changeVideo (targetVid) {
+    // let vids = this.state.videos;
+
+    // let index = vids.indexOf(targetVid);
+
+    this.setState({
+      video: targetVid
+      // videos: vids.slice(0, index).concat(vids.slice(index + 1))
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.video}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList cv={this._changeVideo.bind(this)} videos={this.state.videos}/>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
