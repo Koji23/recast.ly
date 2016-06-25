ed4145329df500596edd3af5156f2ddd2fb1e150
Koji23 <jordanchong23@gmail.com>
2016-06-24 21:59:45 -0700

    Working on searchYoutube function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,17 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = ({query = 'kanye west', max = 5, key = 'AIzaSyAnrZXeqOxSVkRef0Fa9nKLXugFM-jEiiE'}, callback) => {
+  //url
+  var url = "https://www.googleapis.com/youtube/v3/videos?";
+  url += query.split(' ').join('%20') + '&';
+  url += 'maxResults=' + max + '&';
+  // url += 'videoEmbeddable=true' + '&';
+  url += 'key=' + key;
+
+  $.get(url, callback);
 };
 
 window.searchYouTube = searchYouTube;
+
+searchYouTube({}, function(data) {
+  console.log(JSON.stringify(data));
+});
+
